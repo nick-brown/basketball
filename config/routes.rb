c03c362bc1a77b3c99dbe37e6c8d31be073fae5f
Nick Brown <jellatin@gmail.com>
2013-01-22 23:30:25 -0700

    team displaying with division name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Associations::Application.routes.draw do
+
+  resources :divisions
+
+
+  resources :teams
+
+
+  get "team/create"
+
+  get "team/show"
+
+  get "team/destroy"
+
+  get "team/edit"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +63,7 @@ Associations::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  teams = Team.create([
+                        { city: 'San Antonio', name: 'Spurs' },
+                        { city: 'Cleveland', name: 'Cavaliers' },
+                        { city: 'Los Angeles', name: 'Lakers' },
+                        { city: 'Phoenix', name: 'Suns' }
+                      ])
+
+  division = Division.create([
+                        { name: 'East' },
+                        { name: 'West' }
+                             ])
